fed644c31f46d30e7c2d4f6a8efeb9ccb8406f5f
Claire Price <claire.price@mail.com>
2024-06-10 21:27:51 +0100

    remove commented out code referencing bastion host

--- main.tf
+++ main.tf
@@ -23,16 +23,6 @@ provider "aws" {
   region  = "eu-north-1"
 }
 
-# provider "tls" {}
-
-# provider "local" {}
-
-# provider "http" {}
-
-# data "http" "myip" {
-#   url = "https://ipinfo.io/json"
-# }
-
 module "vpc" {
   source = "./vpc"
 
@@ -43,172 +33,10 @@ module "vpc" {
   app_name              = var.app_name
 }
 
-# resource "aws_vpc" "main" {
-#   cidr_block       = "10.0.0.0/24"
-#   instance_tenancy = "default"
-
-#   tags = {
-#     Name = "cwc-vpc"
-#   }
-# }
-
-# resource "aws_subnet" "public-subnet" {
-#   vpc_id     = aws_vpc.main.id
-#   cidr_block = "10.0.0.0/25"
-
-#   availability_zone = "eu-north-1a"
-
-#   tags = {
-#     Name = "cwc-public-subnet"
-#   }
-# }
-
-# resource "aws_subnet" "private-subnet" {
-#   vpc_id     = aws_vpc.main.id
-#   cidr_block = "10.0.0.128/25"
-
-#   availability_zone = "eu-north-1a"
-
-#   tags = {
-#     Name = "cwc-private-subnet"
-#   }
-# }
-
-# resource "aws_internet_gateway" "gw" {
-#   vpc_id = aws_vpc.main.id
-
-#   tags = {
-#     Name = "cwc-igw"
-#   }
-# }
-
-# resource "aws_route_table" "public-route-table" {
-#   vpc_id = aws_vpc.main.id
-
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.gw.id
-#   }
-
-#   tags = {
-#     Name = "cwc-public-route-table"
-#   }
-# }
-
-# resource "aws_route_table_association" "public_subnet_association" {
-#   subnet_id      = aws_subnet.public-subnet.id
-#   route_table_id = aws_route_table.public-route-table.id
-# }
-
-# resource "aws_eip" "nat" {
-#   vpc = true
-
-#   depends_on = [aws_internet_gateway.gw]
-# }
-
-# resource "aws_nat_gateway" "gw_nat" {
-#   allocation_id = aws_eip.nat.id
-#   subnet_id     = aws_subnet.public-subnet.id
-
-#   tags = {
-#     Name = "cwc-natgw"
-#   }
-
-#   depends_on = [aws_internet_gateway.gw]
-# }
-
-# resource "aws_route_table" "private-route-table" {
-#   vpc_id = aws_vpc.main.id
-
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_nat_gateway.gw_nat.id
-#   }
-
-#   tags = {
-#     Name = "cwc-private-route-table"
-#   }
-# }
-
-# resource "aws_route_table_association" "private_subnet_association" {
-#   subnet_id      = aws_subnet.private-subnet.id
-#   route_table_id = aws_route_table.private-route-table.id
-# }
-
-# resource "tls_private_key" "private_key" {
-#   algorithm = "RSA"
-#   rsa_bits  = 4096
-# }
-
-# resource "aws_key_pair" "key" {
-#   key_name   = "cwc-key"
-#   public_key = tls_private_key.private_key.public_key_openssh
-# }
-
-# resource "local_file" "private_key" {
-#   content  = tls_private_key.private_key.private_key_pem
-#   filename = "cwc-key.pem"
-# }
-
-# resource "aws_security_group" "cwc_public_sg" {
-#   name        = "cwc_public_sg"
-#   description = "Allow SSH and TCP inbound traffic and all outbound traffic"
-#   vpc_id      = module.vpc.vpc_id
-
-#   ingress {
-#     from_port       = 22
-#     to_port         = 22
-#     protocol        = "tcp"
-#     cidr_blocks     = [format("%s/32", jsondecode(data.http.myip.response_body).ip)]
-#   }
-
-#   ingress {
-#     from_port       = 80
-#     to_port         = 80
-#     protocol        = "tcp"
-#     cidr_blocks     = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port       = 0
-#     to_port         = 0
-#     protocol        = "-1"
-#     cidr_blocks     = ["0.0.0.0/0"]
-#   }
-
-#   tags = {
-#     Name = "cwc_public_sg"
-#   }
-# }
-
-# resource "aws_instance" "public_instance" {
-#   ami           = "ami-0d3a2960fcac852bc"
-#   instance_type = "t3.micro"
-
-#   subnet_id   = module.vpc.public_subnet_ids[0]
-
-#   associate_public_ip_address = true
-
-#   key_name = aws_key_pair.key.key_name
-
-#   vpc_security_group_ids = [aws_security_group.cwc_public_sg.id]
-
-#   tags = {
-#     Name = "cwc-public-ec2"
-#   }
-# }
-
 resource "aws_security_group" "cwc_private_sg" {
   name        = "cwc_private_sg"
   description = "TCP inbound traffic from public subnet, and all outbound traffic"
   vpc_id      = module.vpc.vpc_id
-
-  # ingress {
-  #   from_port       = 22
-  #   to_port         = 22
-  #   protocol        = "tcp"
-  #   security_groups = [aws_security_group.cwc_bastion_host_sg.id]
-  # }
 
   ingress {
     from_port       = 80
@@ -238,8 +66,6 @@ resource "aws_instance" "private_instance" {
 
   associate_public_ip_address = false
 
-  # key_name = aws_key_pair.key.key_name
-
   vpc_security_group_ids = [aws_security_group.cwc_private_sg.id]
 
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
@@ -250,47 +76,6 @@ resource "aws_instance" "private_instance" {
     Name = "${var.app_name}-private-ec2-${count.index + 1}"
   }
 }
-
-# resource "aws_security_group" "cwc_bastion_host_sg" {
-#   name        = "cwc_bastion_host_sg"
-#   description = "Allow SSH traffic from my IP address and all outbound traffic"
-#   vpc_id      = module.vpc.vpc_id
-
-#   ingress {
-#     from_port       = 22
-#     to_port         = 22
-#     protocol        = "tcp"
-#     cidr_blocks     = [format("%s/32", jsondecode(data.http.myip.response_body).ip)]
-#   }
-
-#   egress {
-#     from_port       = 0
-#     to_port         = 0
-#     protocol        = "-1"
-#     cidr_blocks     = ["0.0.0.0/0"]
-#   }
-
-#   tags = {
-#     Name = "cwc_bastion_host_sg"
-#   }
-# }
-
-# resource "aws_instance" "bastion_host" {
-#   ami           = "ami-0d3a2960fcac852bc"
-#   instance_type = "t3.micro"
-
-#   subnet_id   = module.vpc.public_subnet_ids[0]
-
-#   associate_public_ip_address = true
-
-#   key_name = aws_key_pair.key.key_name
-
-#   vpc_security_group_ids = [aws_security_group.cwc_bastion_host_sg.id]
-
-#   tags = {
-#     Name = "cwc-bastion-host-ec2"
-#   }
-# }
 
 resource "aws_iam_role" "ec2_role" {
   name = "${var.app_name}-ec2-role"
